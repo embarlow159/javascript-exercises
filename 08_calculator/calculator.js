0da8850d544c063703bc7f32a5a1bb6604aa9521
Emma Barlow <e.barlow159@gmail.com>
2023-07-30 11:56:54 +0100

    exercise 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(value1, value2) {
+  return value1+value2
 };
 
-const subtract = function() {
-	
+const subtract = function(value1, value2) {
+	return value1-value2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i=0; i<arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let sum = 1;
+  for (let i=0; i<args.length; i++) {
+    sum = sum*args[i];
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+	return Math.pow(value1, value2)
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+	if (value<0) {
+    return -1;
+  }
+  else if (value===0) {
+    return 1;
+  }
+  else {
+    return (value * factorial(value - 1))
+  }
 };
 
 // Do not edit below this line
