082841c999be5c17679cad81323875e7d9b17908
Emma Barlow <e.barlow159@gmail.com>
2023-07-31 19:19:34 +0100

    exercise 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let processWord = word.toLowerCase().replace(/[^A-Za-z0-9]/g, '');
+    let splitString = processWord.split('');
+    let reverseString = splitString.reverse();
+    let joinString = reverseString.join('');
+    if (processWord===joinString) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
